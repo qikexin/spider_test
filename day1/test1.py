0bc7570f27788289146179313008cd9ccd55b032
lpw <442406570@qq.com>
2019-01-19 13:42:06 +0800

    day1/test

---
+++ day1/test1.py
@@ -0,0 +1,3 @@
+# -*- coding:utf-8 -*-
+# Author:lpw
+# Time: 2019/1/19 13:41
--- spider1/spider1/spiders/xiaohuar.py
+++ spider1/spider1/spiders/xiaohuar.py
@@ -1,12 +1,25 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import  import sys,os,io
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='gb18030')
-from scrapy.selector import HtmlXPathSelector
+# import sys,os,io
+# sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='gb18030')
+from scrapy.selector import HtmlXPathSelector,Selector
+from scrapy.http.request import Request
+from scrapy.http.cookies import CookieJar
 class XiaohuarSpider(scrapy.Spider):
     name = 'xiaohuar'
     allowed_domains = ['http://www.xiaohuar.com/']
-    start_urls = ['http://http://www.xiaohuar.com//']
+    start_urls = ['http://www.xiaohuar.com//']
 
     def parse(self, response):
-        print(response.text)
+        hxs = Selector(response=response)
+        hxa = HtmlXPathSelector(response)
+
+        print(response.url)
+        result = hxs.xpath('//img')
+        print(result)
+
+        # 放到调度器中使用下面固定的规则
+        # for url in result:
+        #     cookie_jar = CookieJar()
+        #     cookie_jar.extract_cookies(response,response.request)
+        #     yield Request(url=url,callable=self.parse)
